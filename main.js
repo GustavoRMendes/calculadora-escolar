4ff4af2cd70566ea902fcd2d0a1910930569ef7d
Gustavo-Mendess <gustavormendess@gmail.com>
2023-11-27 10:22:07 -0400

    consegui

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-let nome = document.getElementById('nome');
-let peso = document.getElementById('nota');
-let peso2 = document.getElementById('notinha');
-let resultado = document.getElementById('resultado');
-let btn = document.getElementById('btn');
-let error = document.getElementById('error')
+let nome = document.getElementById("nome");
+let peso = document.getElementById("nota");
+let peso2 = document.getElementById("notinha");
+let resultado = document.getElementById("resultado");
+let btn = document.getElementById("btn");
+let error = document.getElementById("error");
 
-btn.addEventListener('click',function calcular(e) {
-    e.preventDefault();
-    if(nome.value == '' || peso.value == '' || peso2.value == ''){
-        error.textContent = 'Preencha corretamente!'
-        error.style.color = 'yellow'
-        setTimeout(() =>{
-            error.textContent = ''
-        },2000);
-        return;
-    }else{
-        peso = Number(peso.value)
-        peso2 = Number(peso2.value)
-        let media = peso.value + peso2.value
-        resultado.textContent = `${nome.value}, sua média foi ${media}`
-        resultado.style.color = 'white';
-        peso.value = ''
-        peso2.value = ''
-        nome.value = ''
-        
-    }
-})
+btn.addEventListener("click", function calcular(e) {
+  e.preventDefault();
+  if (nome.value == "" || peso.value == "" || peso2.value == "") {
+    error.textContent = "Preencha corretamente!";
+    error.style.color = "yellow";
+    setTimeout(() => {
+      error.textContent = "";
+    }, 2000);
+    return;
+  } else if (peso.value > 10 || peso2.value > 10) {
+    error.textContent = "Dados Incorretos!";
+    error.style.color = 'yellow'
+    resultado.textContent = "";
+  } else {
+    let media = (peso.value * 2) + (peso2.value * 2);
+    let mediaReal = media / 4;
+    resultado.textContent = `${nome.value},  sua média é ${mediaReal}`;
+    resultado.style.color = "white";
+    peso.value = "";
+    peso2.value = "";
+    nome.value = "";
+  }
+});
